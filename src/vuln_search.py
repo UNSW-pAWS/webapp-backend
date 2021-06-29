1f3cb912ae2b91273c9dc2062aaccbc52af9feda
Steven Dai <s.dai@student.unsw.edu.au>
2021-06-29 21:45:21 +1000

    handles empty results

--- src/vuln_search.py
+++ src/vuln_search.py
@@ -1,5 +1,14 @@
 import requests
 
+from urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
+#The terms vulnerability and CVE are used interchangeably throughout this document. CVE means Common Vulnerability Enumeration
+
+#The terms product and CPE are used interchangeably throughout this document.
+#CPE means Common Platform Enumeration, version 2.3, a standard for identifying and searching products. Need to include vulnerability cpes/1.0?addOns=cves
+
 def package_list_data(package_dic):
     all_data = {}
     for x in package_dic.keys():
@@ -7,26 +16,45 @@ def package_list_data(package_dic):
     return all_data
 
 def extract_relevant_data(search_word):
-
+    search_word = search_word.replace("-", "+")
     endpoint = "https://services.nvd.nist.gov/rest/json/cves/1.0?keyword=" + f'{search_word}'
     cve_response = requests.get(endpoint, verify=False)
     cve_result = cve_response.json()
+
+    if cve_result['totalResults'] == 0:
+        return []
     cve_result = cve_result['result']['CVE_Items']
     result_list = []
     for x in cve_result:
         dic = {'cve_id': x['cve']['CVE_data_meta']['ID'],
+               ''
                'reference_links': [y['url'] for y in x['cve']['references']['reference_data']],
                'description': [y['value'] for y in x['cve']['description']['description_data']],
                'cve_data_version': x['configurations']['CVE_data_version'],
                'published_date': x['publishedDate'],
                'last_modified_date': x['lastModifiedDate']}
-        if x['impact']:
+        if 'baseMetricV3' in x['impact']:
             dic['base_score'] = x['impact']['baseMetricV3']['cvssV3']['baseScore']
             dic['base_severity'] = x['impact']['baseMetricV3']['cvssV3']['baseSeverity']
             dic['exploitability_score'] = x['impact']['baseMetricV3']['exploitabilityScore']
             dic['impact_score'] =  x['impact']['baseMetricV3']['impactScore']
+        elif 'baseMetricV2' in x['impact']:
+            dic['base_score'] = x['impact']['baseMetricV2']['cvssV2']['baseScore']
+            # dic['base_severity'] = x['impact']['baseMetricV2']['cvssV2']['baseSeverity']
+            dic['exploitability_score'] = x['impact']['baseMetricV2']['exploitabilityScore']
+            dic['impact_score'] = x['impact']['baseMetricV2']['impactScore']
 
         # not sure about versionEndExcluding - might be multiple
         result_list.append(dic)
 
     return result_list
+
+print(package_list_data({'react': 1, 'react-dom': 1, 'react-router-dom': 1, 'loose-envify': 1, 'object-assign': 1, 'scheduler': 1, '@babel/runtime': 1, 'history': 1, 'prop-types': 1, 'react-router': 1, 'tiny-invariant': 1, 'tiny-warning': 1}))
+
+#replace - with + (e.g. prop+types)
+
+
+
+
+
+
