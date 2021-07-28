68d778d48e592e9c9c3286efe3d9227f94ad41a9
Roychai <z5212415@student.unsw.edu.au>
2021-07-29 01:07:21 +1000

    change app to application

--- src/appilcation.py
+++ src/appilcation.py
@@ -7,10 +7,10 @@ from flask_cors import CORS
 from alt_pkgs import ALT_PKGS
 from threat import THREAT
 
-app = Flask(__name__)
-CORS(app)
-app.register_blueprint(ALT_PKGS)
-app.register_blueprint(THREAT)
+application = Flask(__name__)
+CORS(application)
+application.register_blueprint(ALT_PKGS)
+application.register_blueprint(THREAT)
 
 def default_handler(err):
     response = err.get_response()
@@ -23,11 +23,11 @@ def default_handler(err):
     response.content_type = 'application/json'
     return response
 
-app.register_error_handler(Exception, default_handler)
+application.register_error_handler(Exception, default_handler)
 
-@app.route("/")
+@application.route("/")
 def home():
     return "Hello there!"
 
 if __name__ == "__main__":
-    app.run(debug=True, port=(int(sys.argv[1]) if len(sys.argv) == 2 else 9447))
+    application.run(debug=True, port=(int(sys.argv[1]) if len(sys.argv) == 2 else 9447))
