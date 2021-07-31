5ff3b264ce764637eebf179f4ecf1c9f3f63a2d8
Roychai <z5212415@student.unsw.edu.au>
2021-07-31 15:12:15 +1000

    cleanup and added async calls

--- src/vuln_search.py
+++ src/vuln_search.py
@@ -1,5 +1,6 @@
 import requests
-
+import asyncio
+import aiohttp
 from urllib3.exceptions import InsecureRequestWarning
 
 requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
@@ -11,51 +12,57 @@ from package_detail_retrieval import package_detail_retrieval
 #The terms product and CPE are used interchangeably throughout this document.
 #CPE means Common Platform Enumeration, version 2.3, a standard for identifying and searching products. Need to include vulnerability cpes/1.0?addOns=cves
 
-def package_list_data(package_dic, severity="None", date="None"):
+async def package_list_data(package_dic, severity="None", date="None"):
     all_data = {}
     if len(package_dic) == 0:
         return all_data
+    work_queue = asyncio.Queue()
     for x in package_dic.keys():
-        all_data[x] = extract_relevant_data(x, severity, date)
+        await work_queue.put(x)
+    await asyncio.gather(asyncio.create_task(extract_relevant_data(work_queue,severity,date,all_data)),asyncio.create_task(extract_relevant_data(work_queue,severity,date,all_data)),asyncio.create_task(extract_relevant_data(work_queue,severity,date,all_data)),asyncio.create_task(extract_relevant_data(work_queue,severity,date,all_data)),asyncio.create_task(extract_relevant_data(work_queue,severity,date,all_data)))
     return all_data
 
-def extract_relevant_data(search_word, severity="None", date="None"):
-    if search_word == "": return []
+async def extract_relevant_data(work_queue, severity="None", date="None",all_data={}):
+    async with aiohttp.ClientSession() as session:
+        while not work_queue.empty():
+            search_word = await work_queue.get()
+            if search_word == "": return []
 
-    search_word = search_word.replace("-", "+")
-    if date == "None": 
-        endpoint = "https://services.nvd.nist.gov/rest/json/cves/1.0?keyword=" + f'{search_word}'
-    else: 
-        endpoint = "https://services.nvd.nist.gov/rest/json/cves/1.0?modStartDate=" + f'{date}' + "T00:00:00:000 UTC-05:00&keyword=" + f'{search_word}'
-    cve_response = requests.get(endpoint, verify=False)
-    cve_result = cve_response.json()
+            search_word = search_word.replace("-", "+")
+            if date == "None": 
+                endpoint = "https://services.nvd.nist.gov/rest/json/cves/1.0?keyword=" + f'{search_word}'
+            else: 
+                endpoint = "https://services.nvd.nist.gov/rest/json/cves/1.0?modStartDate=" + f'{date}' + "T00:00:00:000 UTC-05:00&keyword=" + f'{search_word}'
+            async with session.get(endpoint) as cve_response:
+                cve_result = await cve_response.json()
 
-    if cve_result['totalResults'] == 0:
-        return []
-    cve_result = cve_result['result']['CVE_Items']
-    result_list = []
-    for x in cve_result:
-        dic = {'cve_id': x['cve']['CVE_data_meta']['ID'],
-               'reference_links': [y['url'] for y in x['cve']['references']['reference_data']],
-               'description': [y['value'] for y in x['cve']['description']['description_data']],
-               'cve_data_version': x['configurations']['CVE_data_version'],
-               'published_date': x['publishedDate'],
-               'last_modified_date': x['lastModifiedDate']}
-        if 'baseMetricV3' in x['impact']:
-            dic['base_score'] = x['impact']['baseMetricV3']['cvssV3']['baseScore']
-            dic['base_severity'] = x['impact']['baseMetricV3']['cvssV3']['baseSeverity']
-            dic['exploitability_score'] = x['impact']['baseMetricV3']['exploitabilityScore']
-            dic['impact_score'] =  x['impact']['baseMetricV3']['impactScore']
-        elif 'baseMetricV2' in x['impact']:
-            dic['base_score'] = x['impact']['baseMetricV2']['cvssV2']['baseScore']
-            # dic['base_severity'] = x['impact']['baseMetricV2']['cvssV2']['baseSeverity']
-            dic['exploitability_score'] = x['impact']['baseMetricV2']['exploitabilityScore']
-            dic['impact_score'] = x['impact']['baseMetricV2']['impactScore']
+                if cve_result['totalResults'] == 0:
+                    all_data[search_word] = []
+                    return
+                cve_result = cve_result['result']['CVE_Items']
+                result_list = []
+                for x in cve_result:
+                    dic = {'cve_id': x['cve']['CVE_data_meta']['ID'],
+                           'reference_links': [y['url'] for y in x['cve']['references']['reference_data']],
+                           'description': [y['value'] for y in x['cve']['description']['description_data']],
+                           'cve_data_version': x['configurations']['CVE_data_version'],
+                           'published_date': x['publishedDate'],
+                           'last_modified_date': x['lastModifiedDate']}
+                    if 'baseMetricV3' in x['impact']:
+                        dic['base_score'] = x['impact']['baseMetricV3']['cvssV3']['baseScore']
+                        dic['base_severity'] = x['impact']['baseMetricV3']['cvssV3']['baseSeverity']
+                        dic['exploitability_score'] = x['impact']['baseMetricV3']['exploitabilityScore']
+                        dic['impact_score'] =  x['impact']['baseMetricV3']['impactScore']
+                    elif 'baseMetricV2' in x['impact']:
+                        dic['base_score'] = x['impact']['baseMetricV2']['cvssV2']['baseScore']
+                        # dic['base_severity'] = x['impact']['baseMetricV2']['cvssV2']['baseSeverity']
+                        dic['exploitability_score'] = x['impact']['baseMetricV2']['exploitabilityScore']
+                        dic['impact_score'] = x['impact']['baseMetricV2']['impactScore']
 
-        if severity == "None" or dic.get("base_severity") in severity: 
-            result_list.append(dic)
-
-    return result_list
+                    if severity == "None" or dic.get("base_severity") in severity: 
+                        result_list.append(dic)
+                all_data[search_word] = result_list
+                
 
 # root_packages = {"react", "node"}
 # my_output = {"react" : ["react-dependency1", "react-dependency2"], "node": ["node-dependency2"], 
